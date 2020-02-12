3dae35c344ddb151f38c61aea9f9d234d757e95f
Brendo Otávio <carvbrendo@gmail.com>
2020-02-12 15:33:52 -0400

    Projeto Unity

---
+++ DesafionasAlturas/Assets/scripts/Diretor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diretor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject imagemGamerOver;
+    private Aviao aviao;
+    
+
+    private void Start()
+    {
+        this.aviao = GameObject.FindObjectOfType<Aviao>();
+    }
+    public void FinalizarJogo()
+    {
+        Time.timeScale = 0;
+        this.imagemGamerOver.SetActive(true);
+       
+    }
+    public void ReiniciarJogo()
+    {
+        this.imagemGamerOver.SetActive(false);
+        Time.timeScale = 1;
+        this.aviao.Reiniciar();
+        this.DestruirObstaculos();
+    }
+    private void DestruirObstaculos ()
+    {
+        Obstaculo[] obstaculos = GameObject.FindObjectsOfType<Obstaculo>();
+        foreach( Obstaculo obstaculo in obstaculos)
+        {
+            obstaculo.Destruir();
+        }
+    }
+}
---
+++ DesafionasAlturas/Assets/scripts/Pontuacao.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pontuacao : MonoBehaviour
+{
+    private int pontos;
+    [SerializeField]
+    private Text textoPontuacao;
+
+    public void AdicionarPontos()
+    {
+        this.pontos++;
+        Debug.Log(this.pontos);
+        this.textoPontuacao.text = this.pontos.ToString();
+    }
+
+}
